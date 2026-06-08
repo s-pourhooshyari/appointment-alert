887ae67f98e2ab748fc32542d4ba9414ca8f6313
s-pourhooshyari <140829117+s-pourhooshyari@users.noreply.github.com>
2026-06-08 18:11:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def home():
             timeout=20
         )
     return "Bot is running!"
-    if __name__ == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
